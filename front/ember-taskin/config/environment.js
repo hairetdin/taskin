e94b2a155ede5801334fe40c5b77288703dfb1f8
hairetdin <amir.hairetdinov@gmail.com>
2017-04-05 14:22:14 +0500

    ember-django-adapter module installed and configuration settings made

--- front/ember-taskin/config/environment.js
+++ front/ember-taskin/config/environment.js
@@ -24,6 +24,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.APP.API_NAMESPACE = 'taskin/api';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -43,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.APP.API_NAMESPACE = 'taskin/api';
   }
 
   return ENV;
