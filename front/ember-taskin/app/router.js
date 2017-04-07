6f7402537b35e450de3ef382b34806dbc602016c
hairetdin <amir.hairetdinov@gmail.com>
2017-04-07 08:26:33 +0500

    The route structure created

--- front/ember-taskin/app/router.js
+++ front/ember-taskin/app/router.js
@@ -11,15 +11,54 @@ Router.map(function() {
     this.route('login');
     this.route('projects', function() {
       this.route('new');
-      this.route('edit');
-      this.route('show');
+      this.route('edit', {
+        path: ':project_id/edit'
+      });
+      this.route('show', {path: ':project_id'}, function() {
+        this.route('taskin.members', {path: "/members", resetNamespace: true}, function() {
+          this.route('new');
+          this.route('show', {
+            path: ':member_id'
+          });
+          this.route('edit', {
+            path: ':member_id/edit'
+          });
+        });
+        this.route('taskin.tasks', {path: "/tasks", resetNamespace: true}, function() {
+          this.route('new');
+
+          this.route('show', {path: ':task_id'}, function() {
+            this.route('taskin.taskcomments', {path: "/comments", resetNamespace: true}, function() {
+              this.route('new');
+            });
+            this.route('taskin.taskfiles', {path: "/files", resetNamespace: true}, function() {
+              this.route('new');
+            });
+          });
+
+          this.route('edit', {
+            path: ':task_id/edit'
+          });
+        });
+        this.route('taskin.taskstatuses', {path: "/taskstatuses", resetNamespace: true}, function() {
+          this.route('new');
+          this.route('show', {
+            path: ':taskstatus_id'
+          });
+          this.route('edit', {
+            path: ':taskstatus_id/edit'
+          });
+        });
+      });
     });
+    /*
     this.route('tasks');
     this.route('members');
     this.route('taskstatuses');
     this.route('taskcomment');
     this.route('taskcomments');
     this.route('taskfiles');
+    */
     this.route('people');
   });
   this.route('login');
